c49b8c9db68a26306d86a5ae01b89d67256db246
Tony Tang <teokigi@hotmail.com>
2020-03-30 21:03:21 +0100

    created a few webpages and added to controller

--- controller.rb
+++ controller.rb
@@ -1,4 +1,29 @@
 require('sinatra')
 require('sinatra/contrib/all') if development?
-require_relative('models/calculator.rb')
+require_relative('./models/rps.rb')
 also_reload('./models/*')
+
+get '/home' do
+end
+get '/rules' do
+end
+get '/game_start' do
+end
+get '/rock' do
+end
+get '/paper' do
+end
+get '/scissors' do
+end
+get '/rock/rock' do
+end
+get '/rock/paper' do
+end
+get '/rock/scissors' do
+end
+get '/paper/rock' do
+end
+get 'paper/paper' do
+end
+get '/paper/scissors' do
+end
